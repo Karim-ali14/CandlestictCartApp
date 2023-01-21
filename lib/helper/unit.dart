3eb2330088f686f7c5df48f0ae075929fd70085e
Karim ali <karim.ali14199314@gmail.com>
2023-01-21 13:29:42 +0200

    init method for handle face and finger ids in unit class

--- lib/helper/unit.dart
+++ lib/helper/unit.dart
@@ -1,4 +1,32 @@
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth/error_codes.dart' as local_auth_error;
 
 class AppUnitHelper {
 
+  static final _localAuthentication = LocalAuthentication();
+
+  static Future<bool> authenticateUser() async {
+
+    bool isAuthorized = false;
+    try {
+      isAuthorized = await _localAuthentication.authenticate(
+        localizedReason: "Please authenticate to see account balance",
+        options: const AuthenticationOptions(
+          biometricOnly: true,
+          useErrorDialogs: true,
+          stickyAuth: false,
+        ),
+      );
+
+    } on PlatformException catch (exception) {
+      if (exception.code == local_auth_error.notAvailable ||
+          exception.code == local_auth_error.passcodeNotSet ||
+          exception.code == local_auth_error.notEnrolled) {
+        isAuthorized = false;
+      }
+    }
+
+    return isAuthorized;
+  }
 }
