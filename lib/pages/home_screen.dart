ce363944c55fce8a985368e869ff0428c0ff0481
Karim ali <karim.ali14199314@gmail.com>
2023-01-21 15:20:37 +0200

    handle show candles data in home screen

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:interactive_chart/interactive_chart.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatefulWidget{
+import '../view_models/candles_view_model.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return HomeScreenState();
-  }
-  
+  _HomeScreenState createState() => _HomeScreenState();
 }
-class HomeScreenState extends State<HomeScreen>{
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<CandleData> candles = [];
+  bool themeIsDark = false;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: ChangeNotifierProvider<CandleViewModel>(
+            create: (context) => CandleViewModel()..fetchCandles(),
+            builder: (context, child) {
+              return Selector<CandleViewModel,List<CandleData>>(
+                  selector: (context, model) => model.candles,
+                  builder: (context, candles, child) {
+                    return candles.isEmpty
+                        ? const CircularProgressIndicator()
+                        : InteractiveChart(
+                      candles: candles,
+                    );
+                  }
+              );
+            }
+        ),
+      ),
+    );
   }
-  
 }
