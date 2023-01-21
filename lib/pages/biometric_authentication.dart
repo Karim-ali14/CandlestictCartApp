a3ac6d0ff03f315733da7789133d6663aea0b06b
Karim ali <karim.ali14199314@gmail.com>
2023-01-21 14:19:20 +0200

    make local auth configration

--- lib/pages/biometric_authentication.dart
+++ lib/pages/biometric_authentication.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../helper/unit.dart';
+
 class BiometricAuthentication extends StatefulWidget {
   const BiometricAuthentication({Key? key})
       : super(key: key);
@@ -10,7 +12,15 @@ class BiometricAuthentication extends StatefulWidget {
 }
 
 class _BiometricAuthenticationState extends State<BiometricAuthentication> {
+  @override
+  void initState() {
+    AppUnitHelper.authenticateUser().then((value) {
+      if (value) {
 
+      }
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
