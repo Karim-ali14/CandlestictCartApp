f808db0e3591ba582daf43e0230fa603b2e4692b
Karim ali <karim.ali14199314@gmail.com>
2023-01-21 13:18:25 +0200

    init BiometricAuthentication to handle auth process

---
+++ lib/pages/biometric_authentication.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class BiometricAuthentication extends StatefulWidget {
+  const BiometricAuthentication({Key? key})
+      : super(key: key);
+
+  @override
+  State<BiometricAuthentication> createState() =>
+      _BiometricAuthenticationState();
+}
+
+class _BiometricAuthenticationState extends State<BiometricAuthentication> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+
+
+}
