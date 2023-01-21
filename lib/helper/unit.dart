aa338278bde74e149c123c5b6eddeece2df0cf4f
Karim ali <karim.ali14199314@gmail.com>
2023-01-21 15:16:06 +0200

    handle get candle

--- lib/helper/unit.dart
+++ lib/helper/unit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/services.dart';
+import 'package:interactive_chart/interactive_chart.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:local_auth/error_codes.dart' as local_auth_error;
 
@@ -29,4 +30,24 @@ class AppUnitHelper {
 
     return isAuthorized;
   }
+
+  static List<CandleData> parseCandles(String body) {
+    final List<String> lines = body.split("\n");
+    lines.removeAt(0);
+    final List<CandleData> candles = [];
+    for (var element in lines) {
+      final List<String> values = element.split(",");
+      print("result Of date ${DateTime.parse(values[0].toString())}");
+      candles.add(CandleData(
+        timestamp: DateTime.parse(values[0]).millisecondsSinceEpoch,
+        open: double.parse(values[1]),
+        high: double.parse(values[2]),
+        low: double.parse(values[3]),
+        close: double.parse(values[4]),
+        volume: double.parse(values[5]),
+      ));
+    }
+    return candles;
+  }
+
 }
--- lib/view_models/candles_view_model.dart
+++ lib/view_models/candles_view_model.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/foundation.dart';
+import 'package:interactive_chart/interactive_chart.dart';
+
+import '../commin/constants.dart';
+import '../helper/unit.dart';
+import '../service/service_api.dart';
 
 class CandleViewModel extends ChangeNotifier {
+  List<CandleData> _candles = [];
+  List<CandleData> get candles => _candles;
 
+  void setCandles(List<CandleData> candles) {
+    _candles = candles;
+    notifyListeners();
+  }
+
+  Future<void> fetchCandles({String? period1, String? period2, String? interval}) async {
+    final res = await ServiceApi.fetchCandles(
+        Constants.ticker, period1 ?? Constants.period1, period2 ?? Constants.period2, interval ?? Constants.interval, Constants.events, Constants.crumb
+    );
+    setCandles(AppUnitHelper.parseCandles(res));
+  }
 }
