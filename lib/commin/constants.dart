c6e3d8a4d5c91d2639a64e52e1fdbc04c75ded43
Karim ali <karim.ali14199314@gmail.com>
2023-01-21 13:14:03 +0200

    add baseUrl and used prams

--- lib/commin/constants.dart
+++ lib/commin/constants.dart
@@ -1,4 +1,11 @@
 
 class Constants {
+  static const String baseUrl = "https://query1.finance.yahoo.com/v7/finance/download/";
+  static const String ticker = "SPUS";
+  static const String period1 = "1633381200";
+  static const String period2 = "1674246138";
+  static const String interval = "1wk";
+  static const String events = "history";
+  static const String crumb = "5YTX%2FgVGBmg";
 
 }
