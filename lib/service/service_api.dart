0ab198162c5a3ec4041941285db15bd29037c784
Karim ali <karim.ali14199314@gmail.com>
2023-01-21 15:11:22 +0200

    fetch Candles

--- lib/service/service_api.dart
+++ lib/service/service_api.dart
@@ -1,5 +1,20 @@
+import 'package:http/http.dart' as http;
+import '../commin/constants.dart';
 
 class ServiceApi {
 
+  static Future<String> fetchCandles(
+      String ticker , String period1 , String period2 , String interval
+      ,String events, String crumb) async {
+    final res = await http.get(
+        Uri.parse("${Constants.baseUrl}$ticker?period1=$period1&period2=$period2&interval=$interval&events=$events&crumb=$crumb")
+    );
+    if (res.statusCode == 200) {
+      return res.body;
+    } else {
+      throw Exception('Failed to load candles');
+    }
+
+  }
 
 }
